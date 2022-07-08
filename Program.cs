8ac58ba65e97ed3ef3e78548815c5f7096c637ef
= <wildman700@gmail.com>
2022-07-08 11:34:51 -0600

    yo

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using DinoGame2.Game.Directing;
 using DinoGame2.Game.Scripting;
 using DinoGame2.Game.Services;
+using Raylib_cs;
 
 
 namespace DinoGame2.Game
@@ -21,7 +22,7 @@ namespace DinoGame2.Game
             Cast cast = new Cast();
             cast.AddActor("dino", new Dino());
             cast.AddActor("enemy", new Enemy());
-            cast.AddActor("enemy", new Enemy());
+            //cast.AddActor("enemy", new Enemy());
             cast.AddActor("score", new Score());
             cast.AddActor("goal", new Goal());
 
@@ -34,6 +35,13 @@ namespace DinoGame2.Game
             script.AddAction("update", new Handle_collision());
             script.AddAction("update", new Movement());
             script.AddAction("output", new Draw_actors(videoService));
+
+            Raylib.BeginDrawing();
+            //Raylib_cs raylib = new Raylib_cs();
+            //VideoService.Image DinoImage = LoadImage("DinoGame2_Complete/images/CompleteDino.png");
+            Texture2D player = Raylib.LoadTexture("DinoGame2_Complete/images/CompleteDino.png");
+            Raylib.DrawTexture(player, Constants.DinoSpawn.GetX(), Constants.DinoSpawn.GetY(), Raylib_cs.Color.WHITE);
+            // UnloadTexture(player);
 
             //start the game
             Director director = new Director(videoService);
